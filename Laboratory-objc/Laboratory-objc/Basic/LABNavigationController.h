3458d2db0e0ab41b6bf8b8b08b8d3b6467883cf9
huangqing <huangqing@qeeniao.com>
2019-01-22 10:37:35 +0800

    新增基础视图控制器；

---
+++ Laboratory-objc/Laboratory-objc/Basic/LABNavigationController.h
@@ -0,0 +1,17 @@
+//
+//  LABNavigationController.h
+//  Laboratory-objc
+//
+//  Created by qeeniao35 on 2019/1/22.
+//  Copyright © 2019 CodeZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LABNavigationController : UINavigationController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Laboratory-objc/Laboratory-objc/Basic/LABNavigationController.m
@@ -0,0 +1,32 @@
+//
+//  LABNavigationController.m
+//  Laboratory-objc
+//
+//  Created by qeeniao35 on 2019/1/22.
+//  Copyright © 2019 CodeZ. All rights reserved.
+//
+
+#import "LABNavigationController.h"
+
+@interface LABNavigationController ()
+
+@end
+
+@implementation LABNavigationController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
